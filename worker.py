0ef863400bd5bf020a5e3cf0f47510740215e07a
blacktrub <lightlocdog@gmail.com>
2021-07-07 21:22:04 +0300

    add logging and try/except to fetch errors

--- worker.py
+++ worker.py
@@ -1,15 +1,27 @@
 import time
+import logging
 
 import schedule
 
 from bot import send_message
 from parser import fetch_new_release_links
 
+logging.basicConfig(level=logging.INFO)
+
 
 def fetch_and_send():
-    new_links = fetch_new_release_links()
+    try:
+        new_links = fetch_new_release_links()
+    except Exception:
+        logging.exception("problem with parsing")
+        return
+
     for link in new_links:
-        send_message(link)
+        try:
+            send_message(link)
+        except Exception:
+            logging.exception("problem with telegram")
+
         time.sleep(1)
 
 
